ad640c22747b0e0b282d379d8652b1103d6c9473
Aman Dhungel <aman.most100@gmail.com>
2026-08-11 14:49:59 +0545

    Untrack server folder from repository

--- server/routes/finance/payment.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handle_get_payments } from "../../controllers/finance/payment.controller.js";
-
-const payment_router = express.Router();
-
-payment_router.get("/", handle_get_payments);
-
-export default payment_router;
--- server/routes/step.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handle_get_steps } from "../controllers/step.controller.js";
-
-const step_router = express.Router();
-
-step_router.get("/", handle_get_steps);
-
-export default step_router;
